7f774a0cdad9e2066e160cd02cfa13e004e07b25
doncivo <ali.s.cherif@gmail.com>
2026-05-16 16:20:58 +0100

    Add files via upload

---
+++ trackfit-pwa/sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'trackfit-v3';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll(['/', '/index.html', '/manifest.json', '/icon-180.svg', '/icon-512.svg']);
+    })
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res.status === 200) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached || new Response('Offline', { status: 503 }));
+    })
+  );
+});
